06689e102fcec3e4c184ec869fb21320c3ed42c7
Paulo Welton <paulinho.welton08@gmail.com>
2026-01-22 12:55:44 -0300

    mudanca na rota stocks details e mudanca na pagina de stock details

--- frontend/src/app/features/stock-details/stock-details.ts
+++ frontend/src/app/features/stock-details/stock-details.ts
@@ -3,26 +3,29 @@ import { ActivatedRoute } from '@angular/router';
 import { Header } from "../../core/layout/header/header";
 import { Footer } from "../../core/layout/footer/footer";
 import { StocksService } from '../../core/services/stocks-service';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
+import { AsyncPipe, NgIf, NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-stock-details',
-  imports: [Header, Footer],
+  imports: [Header, Footer, AsyncPipe, NgIf, NgClass],
   templateUrl: './stock-details.html',
   styleUrl: './stock-details.css',
 })
-export class StockDetails {
-  stockDetail$ = new BehaviorSubject<any[]>([]);
+export class StockDetails implements OnInit{
   ticker!: string;
+  stockDetail$!: Observable<any>;
 
-  constructor(private route: ActivatedRoute, private stockService: StocksService) {}
+  constructor(
+    private route: ActivatedRoute,
+     private stockService: StocksService
+  ) {}
 
   ngOnInit(): void {
     this.ticker = this.route.snapshot.paramMap.get('ticker')!;
     
-    this.stockService.getStockDetail(this.ticker).subscribe(data => {
-      this.stockDetail$.next(data)
-      console.log(data)
-    })
+    this.stockDetail$ = this.stockService.getStockDetail(this.ticker).pipe(
+      map(data => data[0])
+    )
   }
 }
